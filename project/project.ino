97fb0a206d6fa78c44044e0873708929666ed316
abal04 <abdulelah.aljendi12345@gmail.com>
2025-04-27 13:46:14 +0200

    Update project.ino
    vi använder debuging för att närmare se vad problemet är exakt

--- project/project.ino
+++ project/project.ino
@@ -1,67 +1,21 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <ArduinoJson.h>
-#include <TFT_eSPI.h>
-#include "pin_config.h"
 
 const char* ssid = "Abo Hasan";
 const char* password = "12345678";
 
-TFT_eSPI tft = TFT_eSPI();
-
-unsigned long lastUpdate = 0;
-const unsigned long updateInterval = 10 * 60 * 1000; // 10 minuter
-
-void showMessage(const String& message, uint32_t color = TFT_WHITE) {
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(color, TFT_BLACK);
-  tft.setTextSize(2);
-  tft.drawString(message, 10, 50);
-}
-
 void setup() {
   Serial.begin(115200);
-  Serial.println("Startar setup...");
-
-  tft.init();
-  tft.setRotation(1);
-  showMessage("Startar...");
-
-  delay(500);
 
   WiFi.begin(ssid, password);
   Serial.println("Ansluter till WiFi...");
-  showMessage("Ansluter WiFi...");
 
-  unsigned long startAttemptTime = millis();
-
-  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 15000) {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("WiFi anslutning misslyckades!");
-    showMessage("WiFi fel!", TFT_RED);
-    while (true); // Stoppa allt
-  }
-
-  Serial.println("WiFi ansluten!");
-  showMessage("WiFi ansluten!", TFT_GREEN);
-  delay(1000);
-
-  showMessage("Hej students");
-  delay(2000);
-  showMessage("Group 9");
-  delay(3000);
-
-  fetchAndShowTemperature();
-  lastUpdate = millis();
-}
-
-void fetchAndShowTemperature() {
-  Serial.println("Försöker hämta SMHI-data...");
-  showMessage("Uppdaterar...");
+  Serial.println("\nWiFi ansluten!");
 
   HTTPClient http;
   http.begin("http://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/15.5869/lat/56.1612/data.json");
@@ -73,57 +27,15 @@ void fetchAndShowTemperature() {
 
   if (httpCode == 200) {
     String payload = http.getString();
-    DynamicJsonDocument doc(40000);  // Ökat minne
-    DeserializationError error = deserializeJson(doc, payload);
-
-    if (!error) {
-      JsonArray timeSeries = doc["timeSeries"];
-      if (!timeSeries.isNull()) {
-        bool found = false;
-        for (JsonObject timeStep : timeSeries) {
-          JsonArray parameters = timeStep["parameters"];
-          for (JsonObject param : parameters) {
-            if (param["name"] == "t") {
-              float temp = param["values"][0];
-              Serial.print("TEMPERATUR: ");
-              Serial.println(temp);
-
-              tft.fillScreen(TFT_BLACK);
-              tft.setTextColor(TFT_WHITE, TFT_BLACK);
-              tft.setTextSize(2);
-              tft.drawString("Plats: Karlskrona", 10, 30);
-              tft.drawString("Temp: " + String(temp, 1) + " \xB0C", 10, 70);
-
-              found = true;
-              break;
-            }
-          }
-          if (found) break;
-        }
-        if (!found) {
-          Serial.println("Hittade ingen temperaturdata!");
-          showMessage("Ingen temperatur!", TFT_RED);
-        }
-      } else {
-        Serial.println("Ingen 'timeSeries' i svaret.");
-        showMessage("Fel i datan!", TFT_RED);
-      }
-    } else {
-      Serial.print("JSON Fel: ");
-      Serial.println(error.c_str());
-      showMessage("JSON Fel!", TFT_RED);
-    }
+    Serial.println("Här är svaret från SMHI:");
+    Serial.println(payload);   // ⚡⚡⚡ Här skriver vi ut ALLT ⚡⚡⚡
   } else {
-    Serial.println("HTTP FEL!");
-    showMessage("HTTP Fel!", TFT_RED);
+    Serial.println("Kunde inte hämta från SMHI.");
   }
 
   http.end();
 }
 
 void loop() {
-  if (millis() - lastUpdate >= updateInterval) {
-    fetchAndShowTemperature();
-    lastUpdate = millis();
-  }
+  // Tom
 }
